737d0d54ea7580313cb4d548a8084c2c775fcae2
eyama <eymtmhk@gmail.com>
2023-07-04 14:13:16 +0900

    Revert "fix: データの取り込み"

--- src/constants/api.ts
+++
@@ -1,2 +0,0 @@
-import "server-only";
-export const apiUrl = "http://127.0.0.1:3000/api";
--- src/libs/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "microcms-js-sdk";
-
-const API_KEY = process.env.NEXT_PUBLIC_CMS_API_KEY || "";
-
-export const client = createClient({
-  serviceDomain: "nextjs-blog-eyama",
-  apiKey: API_KEY,
-});
