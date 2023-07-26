7fa9af931b3fb7324efb00fe42c7274ae9c53c65
eyama-tomohiko <eymtmhk@gmail.com>
2023-07-26 15:16:45 +0900

    fix: layout

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,18 +1,15 @@
 import "server-only";
 import BlogList from "../components/Blog";
 import { apiUrl } from "@/constants/api";
-import BlogLayout from "./layout";
 
 export default async function Page() {
   // 2. APIを用いたデータ取得
   const data = await getBlogs();
   return (
-    <BlogLayout>
-      <section>
-        <h2 className="mb-6 text-gray-400 text-xs">Blogs</h2>
-        <BlogList initialState={data} />
-      </section>
-    </BlogLayout>
+    <>
+      <h2 className="mb-6 text-gray-400 text-xs">Blogs</h2>
+      <BlogList initialState={data} />
+    </>
   );
 }
 
